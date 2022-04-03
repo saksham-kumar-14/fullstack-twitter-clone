5eb86d42034801a42bf7dc40053accf2d3e9ec9d
Saksham Kumar <90552451+saksham-kumar-14@users.noreply.github.com>
2022-04-03 16:52:30 +0530

    Update index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,9 +5,35 @@ import RegisteredHome from '../components/Home/registered_home';
 
 const Home = () => {
   const [loggedin, set_loggedin] = useState(false);
-  useEffect(()=>{
-    if(document.cookie!==""){
-      set_loggedin(true)
+  
+  function decodeCookie(token){
+    let start = 0;
+    for(let i=0;i<token.length;i++){
+      if(token[i]==="="){
+        start = i+1;
+        break;
+      }
+    }
+
+    return token.slice(start,token.length)
+  }
+  
+  useEffect(async()=>{
+    try{
+      const token = decodeCookie(document.cookie);
+      const user = jwt.decode(token);
+      const res = await axios.post("http://localhost:3001/login",{
+        email : user.email,
+        password : user.password
+      })
+      const data = await res.data;
+
+      console.log(data)
+      if(data.user){
+        set_loggedin(true)
+      }
+    }catch(error){
+      console.log(error)
     }
   },[])
 
